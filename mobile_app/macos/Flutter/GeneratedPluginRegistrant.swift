30b7715fb6b73fc4998381039e38d18656392f31
BbatoolTariq <batooltariq786@gmail.com>
2026-05-11 10:16:20 +0500

    Replace mobile_app folder with updated version

--- mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import firebase_core
 import firebase_database
 import local_auth_darwin
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
